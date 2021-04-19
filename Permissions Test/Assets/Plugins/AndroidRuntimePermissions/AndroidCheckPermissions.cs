eed88383b468b8df9b7ce0239725e58183edfe5a
Sean Griffiths <this1wasnttaken@live.com>
2021-04-18 19:05:06 -0600

    Setup

---
+++ Permissions Test/Assets/Plugins/AndroidRuntimePermissions/AndroidCheckPermissions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidCheckPermissions : MonoBehaviour
+{
+    Action <bool> permissionSuccess;
+
+    public void RequestPermissions()
+    {
+        RequestPermissions((response) => {
+
+            Debug.LogError("Permission Accepted" + response);
+        });
+    }
+
+    public void RequestPermissions(Action<bool> actionToDo)
+    {
+        this.permissionSuccess = actionToDo;
+
+        AndroidRuntimePermissions.Permission result = AndroidRuntimePermissions.RequestPermission("android.permission.WRITE_EXTERNAL_STORAGE");
+
+        if (result == AndroidRuntimePermissions.Permission.Granted) 
+        {
+            Debug.LogError("We have permission to access external storage!");
+            permissionSuccess?.Invoke(true);
+        }
+        else
+        {
+            Debug.LogError("Permission state: " + result);
+            permissionSuccess?.Invoke(false);
+        }
+    }
+}
